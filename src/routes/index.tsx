d00a137be2f7d57132aaa03495ad1b7fee6b128e
Garv2003 <aggarwalgarv0505@gmail.com>
2024-04-16 23:19:44 +0530

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,24 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <>
+      <h1>Welcome to Qwik</h1>
+      <p>
+        Qwik is a fast, lightweight, and modern web framework for building web
+        applications.
+      </p>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Welcome to Qwik",
+  meta: [
+    {
+      name: "description",
+      content: "Qwik site description",
+    },
+  ],
+};
